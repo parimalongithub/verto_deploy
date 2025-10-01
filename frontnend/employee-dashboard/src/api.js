fd40e2b483b79f65d183fb2ec30fb3b6ae8dda47
parimal <parimalpawar12@gmail.com>
2025-10-01 15:11:07 +0530

    Update API_URL to use production endpoint

--- frontnend/employee-dashboard/src/api.js
+++ frontnend/employee-dashboard/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/employees";
+const API_URL = "https://1fcedc69-343e-4e55-a22f-61ddb73639dd.e1-us-east-azure.choreoapps.dev/api/employee";
 
 export const getEmployees = () => axios.get(API_URL);
 export const getEmployee = (id) => axios.get(`${API_URL}/${id}`);
