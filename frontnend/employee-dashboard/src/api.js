698def5dfd2e4cafb57eb645394c2a2cb77bfd5e
parimal <parimalpawar12@gmail.com>
2025-10-01 15:21:20 +0530

    Update api.js

--- frontnend/employee-dashboard/src/api.js
+++ frontnend/employee-dashboard/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://1fcedc69-343e-4e55-a22f-61ddb73639dd.e1-us-east-azure.choreoapps.dev/api/employee";
+const API_URL = "https://1fcedc69-343e-4e55-a22f-61ddb73639dd.e1-us-east-azure.choreoapps.dev/api/employees";
 
 export const getEmployees = () => axios.get(API_URL);
 export const getEmployee = (id) => axios.get(`${API_URL}/${id}`);
